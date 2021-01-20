34771a5a9a55b5784051a9d5a133ceb43f51dd77
Tziporah <tziporawerzberger@gmail.com>
2021-01-20 00:04:24 -0500

    adds root

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   namespace :api do
+    root :to => "sessions#create"
     post "/users" => "users#create"
     patch "/users/:id" => "users#update"
     get "/users" => "users#index"
